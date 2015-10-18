b90a4217763b048e0f0234983b2150371bdb4301
ukiy <ukiy24654@gmail.com>
2015-10-18 14:01:57 +0900

    maybe ok

--- routes/search.js
+++ routes/search.js
@@ -10,11 +10,12 @@ function searchOrder(query){
   var search = {};
   var tag;
   var limit;
-  search.orderDateTime = {};
   if (query.findByOrderDateTimeGTE){
+    search.orderDateTime = search.orderDateTime || {};
     search.orderDateTime.$gte = parseInt(query.findByOrderDateTimeGTE);
   }
   if (query.findByOrderDateTimeLTE){
+    search.orderDateTime = search.orderDateTime || {};
     search.orderDateTime.$lte = parseInt(query.findByOrderDateTimeLTE);
   }
 //user
@@ -25,11 +26,12 @@ function searchOrder(query){
     search.orderItemId = query.findByOrderItemId;
   }
 
-  search.orderQuantity = {};
   if (query.findByOrderQuantityGTE){
+    search.orderQuantity = search.orderQuantity || {};
     search.orderQuantity.$gte = parseInt(query.findByOrderQuantityGTE);
   }
   if (query.findByOrderQuantityLTE){
+    search.orderQuantity = search.orderQuantity || {};
     search.orderQuantity.$lte = parseInt(query.findByOrderQuantityLTE);
   }
 
@@ -37,13 +39,14 @@ function searchOrder(query){
     search.orderState = query.findByOrderState;
   }
 
-  search.orderTags = {};
   if (query.findByOrderTagsIncludeAll){
+    search.orderTags = search.orderTags || {};
     tags = query.findByOrderTagsIncludeAll.split(',');
     search.orderTags.$all =  tags;
   }
   tags = null;
   if (query.findByOrderTagsIncludeAny){
+    search.orderTags = search.orderTags || {};
     tags = query.findByOrderTagsIncludeAny.split(',');
     search.orderTags.$in = tags;
   }
@@ -53,11 +56,12 @@ function searchOrder(query){
   if (query.findByUserCompany){
     search['user.userCompany'] = query.findByUserCompany;
   }
-  search['user.userDiscountRate'] = {};
   if (query.findByUserDiscountRateGTE){
+    search['user.userDiscountRate'] = search['user.userDiscountRate'] || {};
     search['user.userDiscountRate'].$gte = parseInt(query.findByUserDiscountRateGTE);
   }
   if (query.findByUserDiscountRateLTE){
+    search['user.userDiscountRate'] = search['user.userDiscountRate'] || {};
     search['user.userDiscountRate'].$lte = parseInt(query.findByUserDiscountRateLTE);
   }
 
@@ -66,29 +70,32 @@ function searchOrder(query){
     search['item.itemSupplier'] = query.findByItemSupplier;
   }
 
-  search['item.itemStockQuantity'] = {};
   if (query.findByItemStockQuantityGTE){
+    search['item.itemStockQuantity'] = search['item.itemStockQuantity'] || {};
     search['item.itemStockQuantity'].$gte = parseInt(query.findByItemStockQuantityGTE);
   }
   if (query.findByItemStockQuantityLTE){
+    search['item.itemStockQuantity'] = search['item.itemStockQuantity'] || {};
     search['item.itemStockQuantity'].$lte = parseInt(query.findByItemStockQuantityLTE);
   }
 
-  search['item.itemBasePrice'] = {};
   if (query.findByItemBasePriceGTE){
+    search['item.itemBasePrice'] = search['itemBasePrice'] || {};
     search['item.itemBasePrice'].$gte = parseInt(query.findByItemBasePriceGTE);
   }
   if (query.findByItemBasePriceLTE){
+    search['item.itemBasePrice'] = search['itemBasePrice'] || {};
     search['item.itemBasePrice'].$lte = parseInt(query.findByItemBasePriceLTE);
   }
 
-  search['item.itemTags'] = {};
   if (query.findByItemTagsIncludeAll){
+    search['item.itemTags'] = search['item.itemTags'] || {};
     tags = query.findByItemTagsIncludeAll.split(',');
     search['item.itemTags'] = { $all: tags }
   }
   tags = null;
   if (query.findByItemTagsIncludeAny){
+    search['item.itemTags'] = search['item.itemTags'] || {};
     tags = query.findByItemTagsIncludeAny.split(',');
     search['item.itemTags'] = { $in: tags }
   }
@@ -105,6 +112,7 @@ function searchOrder(query){
   console.log(search);
   return Order.find(search).sort({'orderDateTime': -1}).limit(limit).exec(function(err){
     console.log(err);
+
   });
 };
 
