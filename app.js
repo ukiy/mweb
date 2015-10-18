02454f98b4933cf240ad4afb9ffd5c8c8691a7ff
ukiy <ukiy24654@gmail.com>
2015-10-18 16:04:51 +0900

    remove view etc

--- app.js
+++ app.js
@@ -31,8 +31,6 @@ if (process.env.NODE_ENV == 'production'){
 mongoose.connect(conf.mongo.uri, conf.mongo.opt);
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -40,10 +38,8 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
